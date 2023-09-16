92903ca2f89d1757b8dc662060f006e342bc65ed
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-09-16 19:27:42 +0400

    testing global route middleware

--- middleware/authstate.global.ts
+++ middleware/authstate.global.ts
@@ -1,11 +1,19 @@
 import { useMyUserStore } from "~/stores/user";
 
 export default defineNuxtRouteMiddleware(async () => {
+  if (process.server) return;
+
   const supabaseUser = useSupabaseUser();
+  console.log("supabase user: ", supabaseUser.value);
+
   const store = useMyUserStore();
+  console.log("store: ", store);
+
   if (!supabaseUser.value) return;
 
   if (!store.user || supabaseUser.value.id !== store.user.id) {
+    console.log("strating fetch...");
+
     const { data: apiData } = await useFetch("/api/users", {
       method: "get",
       query: {
